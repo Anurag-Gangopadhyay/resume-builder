3a5a67052292118dadfb2356d347e9c4664f9595
Anurag-Gangopadhyay <166293686+Anurag-Gangopadhyay@users.noreply.github.com>
2025-06-11 03:12:10 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// Export a function to access 'mode'
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/resume-builder/' : '/', // 👈 replace with your repo name
+  };
+});
